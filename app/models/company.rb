81532b359368d74f746c7f2e375a8778d0291e42
Michalis Zamparas <mikezaby@skroutz.gr>
2012-10-13 11:30:13 +0300

    basic signup functionality

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,11 @@
 class Company < ActiveRecord::Base
   attr_accessible :domain
+
+  #associations
+  has_many  :users, :through => :companies_users
+  has_many :companies_users, :dependent => :destroy
+
+  #validations
+  validates_presence_of :domain
+  validates_uniqueness_of :domain
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :companies_attributes
+
+  #associations
+  has_many :companies, :through => :companies_users
+  has_many :companies_users
+
+  accepts_nested_attributes_for :companies
 end
