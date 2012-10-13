c5a7714c202fea6821c9b553c018714b7be9f88b
George Chatzigeorgiou <george@skroutz.gr>
2012-10-13 17:32:54 +0300

    Tournament show/edit and index controller functionality

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,9 +1,42 @@
 class TournamentsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :fetch_tournament, :only => [:show, :edit]
+  before_filter :authorize, :only => [:show, :edit]
 
   # GET /index
   def index
     @tournaments = Tournament.for_user(current_user).
                               order("tournaments.created_at desc")
+  end
+
+  # GET /show/id
+  def show
+  end
+
+  # GET new
+  def new
+    @tournament = Tournament.new
+  end
+
+  # GET edit
+  def edit
+  end
+
+  private
+  def fetch_tournament
+    @tournament = Tournament.find(params[:id])
+  end
+
+  def authorize
+    # can user see this?
+    # TODO: cancan this
+    if @tournament.owner_id != current_user.id &&
+      TournamentUser.where(tournament_id: @tournament.id,
+                           user_id: current_user.id).count == 0
+      flash[:alert] = "You are not supposed to be here"
+      redirect_to tournaments_path
+      return false
+    end
+    true
   end
 end
--- spec/controllers/tournaments_controller_spec.rb
+++ spec/controllers/tournaments_controller_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 describe TournamentsController do
+  # TODO: change to factory girl
+  let(:user){User.create({:email => 'someone@someone.com',
+                          :password => 'foobar'})}
+  let(:another_user){User.create({:email => 'someoneelse@someone.com',
+                                  :password => 'foobar'})}
 
   describe "GET index" do
-    # TODO: change to factory girl
-    let(:user){User.create({:email => 'someone@someone.com',
-                         :password => 'foobar'})}
-    let(:another_user){User.create({:email => 'someoneelse@someone.com',
-                         :password => 'foobar'})}
-
     before do
       login_user(user)
     end
@@ -46,5 +45,114 @@ describe TournamentsController do
       end
     end
   end
+
+  describe "GET show" do
+    let(:tournament){FactoryGirl.create(:tournament,{owner: user})}
+    let(:another_tournament){
+      FactoryGirl.create(:tournament,{owner: another_user})
+    }
+
+    before do
+      login_user(user)
+    end
+
+    context "user owns tournament" do
+      it "renders show template" do
+        get :show, id: tournament.id
+        response.should render_template("show")
+      end
+
+      it "fetches tournament" do
+        get :show, id: tournament.id
+        assigns(:tournament).should eq(tournament)
+      end
+    end
+
+    context "user participates in tournament" do
+      before do
+        another_tournament.users << user
+      end
+
+      it "renders show template" do
+        get :show, id: another_tournament.id
+        response.should render_template("show")
+      end
+
+      it "fetches tournament" do
+        get :show, id: another_tournament.id
+        assigns(:tournament).should eq(another_tournament)
+      end
+    end
+
+    context "tournament doesn't belong to user" do
+      it "doesn't render show page" do
+        get :show, id: another_tournament.id
+        response.should redirect_to(tournaments_path)
+      end
+    end
+  end
+
+  describe "GET new" do
+    before do
+      login_user(user)
+    end
+
+    it "renders the new template" do
+      get :new
+      response.should render_template("new")
+    end
+
+    it "initializes a new tournament" do
+      get :new
+      assigns(:tournament).should be_new_record
+    end
+  end
+
+  describe "GET edit" do
+    let(:tournament){FactoryGirl.create(:tournament,{owner: user})}
+    let(:another_tournament){
+      FactoryGirl.create(:tournament,{owner: another_user})
+    }
+
+    before do
+      login_user(user)
+    end
+
+    context "user owns tournament" do
+      it "renders show template" do
+        get :edit, id: tournament.id
+        response.should render_template("edit")
+      end
+
+      it "fetches tournament" do
+        get :edit, id: tournament.id
+        assigns(:tournament).should eq(tournament)
+      end
+    end
+
+    context "user participates in tournament" do
+      before do
+        another_tournament.users << user
+      end
+
+      it "renders show template" do
+        get :edit, id: another_tournament.id
+        response.should render_template("edit")
+      end
+
+      it "fetches tournament" do
+        get :edit, id: another_tournament.id
+        assigns(:tournament).should eq(another_tournament)
+      end
+    end
+
+    context "tournament doesn't belong to user" do
+      it "doesn't render show page" do
+        get :edit, id: another_tournament.id
+        response.should redirect_to(tournaments_path)
+      end
+    end
+  end
+
 
 end
