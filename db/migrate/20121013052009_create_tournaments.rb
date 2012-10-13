9385090f452da9260a03fc65d4b244a1f5b64510
Stratos Paraskevaidis <stratosiii@gmail.com>
2012-10-13 18:18:27 +0300

    removing not null restriction from tournaments.description (rhymes)

--- db/migrate/20121013052009_create_tournaments.rb
+++ db/migrate/20121013052009_create_tournaments.rb
@@ -2,7 +2,7 @@ class CreateTournaments < ActiveRecord::Migration
   def change
     create_table :tournaments do |t|
 
-      t.string :description, null: false
+      t.string :description
       t.string :tournament_hash
 
       t.timestamps
