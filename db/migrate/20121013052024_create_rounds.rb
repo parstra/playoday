a5289bc12bcf872cc1c73ce1a2da51de4ba1c47d
Stratos Paraskevaidis <stratosiii@gmail.com>
2012-10-13 19:18:02 +0300

    adding round_number to round

--- db/migrate/20121013052024_create_rounds.rb
+++ db/migrate/20121013052024_create_rounds.rb
@@ -3,6 +3,7 @@ class CreateRounds < ActiveRecord::Migration
     create_table :rounds do |t|
 
       t.integer :tournament_id, null:false
+      t.integer :round_number
 
       t.timestamps
     end
--- spec/factories/round.rb
+++ spec/factories/round.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :round do
     tournament
     active false
+    round_number 1
 
     trait :active do
       active true
