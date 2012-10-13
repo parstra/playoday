2e1c1ccc8ec282bd43aec3c85364956e9e5f89d2
George Chatzigeorgiou <george@skroutz.gr>
2012-10-13 07:28:34 +0300

    Initial commit
    Bare app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playoday::Application.config.secret_token = '0f9a0b7e2b3145ae240c1c2f63d57b1a9d74f8e54e67c2e201b5562b722f9a826e0b4f89913c7d9e9826358f98970a11442219da20715af6a10b969a501d14f1'
