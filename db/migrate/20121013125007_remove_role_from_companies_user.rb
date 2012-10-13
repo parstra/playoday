af5196454e91d682bf49653ef121221ef326d93f
George Chatzigeorgiou <george@skroutz.gr>
2012-10-13 19:31:35 +0300

    Removed obsolete migration

--- db/migrate/20121013125007_remove_role_from_companies_user.rb
+++
@@ -1,9 +0,0 @@
-class RemoveRoleFromCompaniesUser < ActiveRecord::Migration
-  def up
-    remove_column :companies_users, :role
-  end
-
-  def down
-    add_column :companies_users, :role, :integer
-  end
-end
