7eb2557eb836a35a9fd2191e5082d974b0357f36
Michalis Zamparas <mikezaby@skroutz.gr>
2012-10-14 19:00:34 +0300

    add matches score funcionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,11 @@ class Ability
   def initialize(user)
     can :read, Tournament, tournament_users: { user_id: user.id }
     can :manage, Tournament, owner_id: user.id
+
+    can :update, Match do |match|
+      (match.away_player_id ==  user.id || match.home_player_id == user.id ||
+        match.round.tournament.owner_id == user.id) &&
+        (!match.played && match.updated_at > 1.day.ago)
+    end
   end
 end
