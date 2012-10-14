1adaa31183033dc29675e7c40757502b17d1d57c
George Chatzigeorgiou <george@skroutz.gr>
2012-10-14 13:39:51 +0300

    Added a swedish tournament in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
    User.create(email: email, password: 'foobar')
  end
 
-t = Tournament.new({name: 'Ping pong hero', 
+t = Tournament.new({name: 'Ping pong hero',
                    description: "Do you think you have what it takes?",
                    game_type: Tournament::CUP})
 
@@ -34,8 +34,40 @@ round = t.rounds.last
 round.matches.each {|m|
   m.played = true
   m.winner_id = [m.home_player_id, m.away_player_id][m.id.modulo(2)]
+  m.home_score = m.winner_id == m.home_player_id ? 4 : 0
+  m.away_score = m.winner_id == m.away_player_id ? 4 : 0
   m.save!
 }
 
 t.next_round
 t.save
+
+t = Tournament.new({name: 'Tennis Ninja',
+                   description: "Do you think you have what it takes?",
+                   game_type: Tournament::SWEDISH})
+
+t.owner = User.first
+t.save!
+
+User.all.each do |u| t.users << u end
+
+# first round
+t.start
+
+# play 4 rounds
+4.times do
+
+  round = t.rounds.last
+
+  round.matches.each {|m|
+    m.played = true
+    m.winner_id = [m.home_player_id, m.away_player_id][m.id.modulo(2)]
+    m.home_score = m.winner_id == m.home_player_id ? 4 : 0
+    m.away_score = m.winner_id == m.away_player_id ? 4 : 0
+    m.save!
+  }
+
+  # go the second round
+  t.next_round
+  t.save
+end
