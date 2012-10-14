5c40c2563f636bba2686ee72661945e5142f79fd
Michalis Zamparas <mikezaby@skroutz.gr>
2012-10-14 22:08:25 +0300

    redirect from welcome message to tournaments if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :redirect_logged_in_users
+
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
       tournaments_path
+    end
+  end
+
+  private
+
+  def redirect_logged_in_users
+    if user_signed_in? && request.path == "/"
+      redirect_to tournaments_path
     end
   end
 
