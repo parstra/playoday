8ea1946b991e84a1ee7f388fee3f25158d934e9e
George Chatzigeorgiou <george@skroutz.gr>
2012-10-14 10:21:24 +0300

    Basic seed file that creates a cup tournament
    It also starts the tournament.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+["george@skroutz.gr", "mikezaby@gmail.com", "stratosiii@gmail.com",
+ "nemlah@skroutz.gr", "gakos.ioannis@gmail.com", "giorgos.tsiftsis@gmail.com",
+ "alup@aloop.org", "kostas.karachalios@me.com"].each do |email|
+   User.create(email: email, password: 'foobar')
+ end
+
+t = Tournament.new({name: 'Ping pong hero', 
+                   description: "Do you think you have what it takes?",
+                   game_type: Tournament::CUP})
+
+t.owner = User.first
+t.save!
+
+User.all.each do |u| t.users << u end
+t.start
