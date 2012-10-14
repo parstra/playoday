4b6ed275c3f0f146c473f388b31ee5bc9b303eb4
Michalis Zamparas <mikezaby@skroutz.gr>
2012-10-14 10:22:43 +0300

    remove emails field from edit registration and disable the cancel fucntionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Playoday::Application.routes.draw do
-  devise_for :users
+
+  devise_for :users, skip: :registrations
+  #disable canceling account
+  devise_scope :user do
+    resource :registration,
+      only: [:new, :create, :edit, :update],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'devise/registrations',
+      as: :user_registration do
+      get :cancel
+    end
+  end
 
   resources :tournaments
 
